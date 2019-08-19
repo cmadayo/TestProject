3236cd7e6303e6ce447dec9a87678b3affe38491
cmadayo <49114694+cmadayo@users.noreply.github.com>
2019-08-19 23:03:28 +0900

    Refactoring(use Path module instead of os.path module)

--- training_model.py
+++ training_model.py
@@ -5,6 +5,7 @@ import pickle
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from pathlib import Path
 
 #########################
 # SETTING
@@ -16,38 +17,44 @@ output_pickle = 'testmodel.pickle'                          # output pickle file
 X = []
 y = []
 
-dir_list = os.listdir(train_data_dir)
+print('start reading training data :)')
+p_label_dir = Path(train_data_dir)
+it_dir = p_label_dir.iterdir()
 
-print('start making training data :)')
-for dir in dir_list:
-    file_list_path = os.path.join(train_data_dir, dir)
-    file_list = os.listdir(file_list_path)
-    root, ext = os.path.splitext(dir)
-    for file in file_list:
-        root, ext = os.path.splitext(file)
-        if(ext == '.png'):
-            abs_name = os.path.join(file_list_path, file)
-            image = cv2.imread(abs_name, 0)
-            X.append(image.reshape(-1,))
-            y.append(int(dir))
+for dir in it_dir:
+    if dir.is_dir():
+        p_filedir = Path(dir)
+        it_file = p_filedir.glob('*.png')
+    else:
+        continue
+
+    for file in it_file:
+        file = str(file)
+        image = cv2.imread(file, 0)
+        X.append(image.reshape(-1,))
+        y.append(int(dir.name))
 
 X = np.array(X)
 y = np.array(y)
-print('complete making training data :D')
 
-print('start training :)')
+print('read complete :D')
+
+print('start training :O')
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 model = SVC(kernel='linear', random_state=None)
 model.fit(X_train, y_train)
-print('complete training :)')
+print('complete training :P')
+
+print('culculate accuracy score...')
 pred_train = model.predict(X_train)
 accuracy_train = accuracy_score(y_train, pred_train)
-print('accuracy at training data set： %.2f' % accuracy_train)
+print('accuracy score at training data set： %.2f' % accuracy_train)
 
+print('culculate accuracy score...')
 pred_test = model.predict(X_test)
 accuracy_test = accuracy_score(y_test, pred_test)
-print('accuracy at test data set： %.2f' % accuracy_train)
+print('accuracy score at test data set： %.2f' % accuracy_train)
 
 filepath = os.path.split(__file__)[0]
 uri_pickle = os.path.join(filepath, output_pickle)
